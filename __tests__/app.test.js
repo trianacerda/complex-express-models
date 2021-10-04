5d81a01ed2bb62a2408a7a80c42ef94f203128ca
Triana Cerda <trianaRcerda@gmail.com>
2021-10-04 14:25:38 -0700

    changed import/fromt to const/require + will continue to refactor if any were missed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('demo routes', () => {
   beforeEach(() => {
