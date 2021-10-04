2bc21b3c3deed4a4e3a555c98c94caafad25635c
Triana Cerda <trianaRcerda@gmail.com>
2021-10-04 14:46:49 -0700

    changed animals to species to post a new species first

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,23 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('demo routes', () => {
+describe('complex-express-models-routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('should add a new species', () => {
+    const newSpecies = {
+      Species: 'Amphibians',
+    };
+    return request(app)
+      .post('/api/species')
+      .send(newSpecies)
+      .then((res) => {
+        expect(res.body).toEqual({
+          Species: 'Amphibians',
+        });
+      });
   });
 
   afterAll(() => {
